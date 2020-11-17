cc7fe6b99eba5d1879b45090c0d8656176aac0c3
Guilherme Araujo <gartisk@gmail.com>
2020-11-17 01:13:30 -0300

    6:*2 Pattern Matching Structs

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,11 +2,20 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail ]} = image
+
+    [r, g, b]
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 
 end
